25fe11774fc4ba4ac5087b480ab8f6c684db601e
pbarera1 <phil.barera@gmail.com>
2025-08-12 16:02:24 -0700

    update for vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/bvh-data-viewer/',
+    // base: '/bvh-data-viewer/', only got github pages but no static asset hosting there
     plugins: [react()],
 });
